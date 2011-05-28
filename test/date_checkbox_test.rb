989d31c6196c6257738149c6952c1cbffd568cbc
Matthias Viehweger <kronn@kronn.de>
2011-05-28 12:28:04 +0200

    perform some smoketest on the testingn infrastructure

--- test/date_checkbox_test.rb
+++ test/date_checkbox_test.rb
@@ -1,4 +1,8 @@
 require 'test_helper'
 
 class DateCheckboxTest < ActiveSupport::TestCase
+  # implementation and infratructure details
+  test "schema is loaded" do
+    assert_equal [], PublishedAtPost.all
+  end
 end
