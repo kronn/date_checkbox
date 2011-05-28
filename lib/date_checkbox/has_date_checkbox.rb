43304da7823888f34e3a875b6aad42773d0ae996
Matthias Viehweger <kronn@kronn.de>
2011-05-28 12:34:01 +0200

    add basic reader, writer and present? methods

--- lib/date_checkbox/has_date_checkbox.rb
+++ lib/date_checkbox/has_date_checkbox.rb
@@ -6,6 +6,17 @@ module DateCheckbox
 
     module ClassMethods
       def has_date_checkbox(db_field)
+        method_name = db_field.to_s.sub(/_at$/, '')
+        class_eval do
+          define_method("#{method_name}") do
+          end
+
+          define_method("#{method_name}?") do
+          end
+
+          define_method("#{method_name}=") do |value|
+          end
+        end
       end
     end
   end
--- test/has_date_checkbox_test.rb
+++ test/has_date_checkbox_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class HasDateCheckboxTest < ActiveSupport::TestCase
   def teardown
     @klass = nil
+    @instance = nil
   end
 
   test "a class method for one field is added" do
@@ -10,11 +11,30 @@ class HasDateCheckboxTest < ActiveSupport::TestCase
     assert_equal 1, klass.method(:has_date_checkbox).arity
   end
 
+  test "has_date_checkbox defines a custom reader for the given attribute" do
+    assert_respond_to instance, :published
+    assert_respond_to instance, :published?
+  end
+
+  test "has_date_checkbox defines a custom writer for the given attribute" do
+    assert_respond_to instance, :'published='
+  end
   protected
 
   def klass
     @klass ||= Class.new do
       include DateCheckbox::HasDateCheckbox
     end
+  end
+
+  def instance
+    @instance ||= begin
+                    inst = PublishedAtPost.new
+                    inst.class.send(:include, DateCheckbox::HasDateCheckbox)
+                    inst.class.class_eval do
+                      has_date_checkbox :published_at
+                    end
+                    inst
+                  end
   end
 end
